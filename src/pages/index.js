e705607fd54864996efaf479e0b9bf72c4a120e1
supercobra <daniel@metadot.com>
2022-01-26 11:16:27 -0600

    Meta tag update.

--- src/pages/index.js
+++ src/pages/index.js
@@ -67,7 +67,8 @@ function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description={`Hello from ${siteConfig.title}`}
+      >
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
